dcfd8c9ff1db506685b73494aeab130e0d7a5bb9
ttotti0903 <144639644+ttotti0903@users.noreply.github.com>
2023-10-10 17:26:06 +0900

    Revert "게시물 DB저장 및 노출 업데이트"

--- teampro/src/main/java/DCS/DCSspring/repository/ArticleRepository.java
+++
@@ -1,7 +0,0 @@
-package DCS.DCSspring.repository;
-
-import DCS.DCSspring.entity.Article;
-import org.springframework.data.repository.CrudRepository;
-
-public interface ArticleRepository extends CrudRepository<Article, Long> {
-}
