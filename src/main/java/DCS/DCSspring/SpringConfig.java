5ba59219f75bd6ac3a4a78d46ac23f81e862220a
정규철 <ttotti0903@naver.com>
2023-10-24 17:04:13 +0900

    게시물 작성 후 후 마감 시간 출력 + 메모리 작동 성공

--- src/main/java/DCS/DCSspring/SpringConfig.java
+++ src/main/java/DCS/DCSspring/SpringConfig.java
@@ -3,9 +3,9 @@ package DCS.DCSspring;
 import DCS.DCSspring.Service.ArticleService;
 import DCS.DCSspring.Service.MemberService;
 import DCS.DCSspring.repository.ArticleRepository;
-import DCS.DCSspring.repository.JdbcTemplateArticleRepository;
-import DCS.DCSspring.repository.JdbcTemplateMemberRepository;
 import DCS.DCSspring.repository.MemberRepository;
+import DCS.DCSspring.repository.MemoryArticleRepository;
+import DCS.DCSspring.repository.MemoryMemberRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,26 +13,23 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-    private final DataSource dataSource;
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
+
     @Bean
     public MemberRepository memberRepository() {
-
-        return new JdbcTemplateMemberRepository(dataSource);
+        return new MemoryMemberRepository(); // 메모리 저장소로 변경
     }
+
     @Bean
-    public ArticleService articleService(){
+    public ArticleService articleService() {
         return new ArticleService(articleRepository());
     }
 
     @Bean
-    public ArticleRepository articleRepository(){
-        return new JdbcTemplateArticleRepository(dataSource);
+    public ArticleRepository articleRepository() {
+        return new MemoryArticleRepository(); // 메모리 저장소로 변경
     }
 }
