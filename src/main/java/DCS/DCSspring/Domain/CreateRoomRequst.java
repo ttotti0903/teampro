b05c393abcd4289247f803fe6bb0783e0384d513
정규철 <ttotti0903@naver.com>
2023-11-11 16:05:28 +0900

    채팅 익명 구현

---
+++ src/main/java/DCS/DCSspring/Domain/CreateRoomRequst.java
@@ -0,0 +1,22 @@
+package DCS.DCSspring.Domain;
+
+public class CreateRoomRequst {
+    private String name;
+    private int maxPeople;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMaxPeople() {
+        return maxPeople;
+    }
+
+    public void setMaxPeople(int maxPeople) {
+        this.maxPeople = maxPeople;
+    }
+}
